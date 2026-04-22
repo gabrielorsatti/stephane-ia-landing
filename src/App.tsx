3d6324a554f53a5ac10c27dc67289e355e712375
gabrielorsatti <gabriel.orsatti@gmail.com>
2026-04-22 17:55:31 +0200

    fix: remove unused imports for production build

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import LandingPage from './LandingPage'
 
 function App() {
   return (
-    <div className="bg-brand min-h-screen flex items-center justify-center">
-      <h1 className="text-white text-4xl font-bold">Gym Track Landing est prête !</h1>
-    </div>
+    <main className="antialiased">
+      <LandingPage />
+    </main>
   )
 }
 
